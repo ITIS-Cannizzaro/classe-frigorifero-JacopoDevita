79fd6e86799593290371f7ab9f45f231fcae85d4
utente <utente@DESKTOP-I4VH3T8>
2023-05-16 12:43:35 +0200

    Frigorifero De vita Jacopo

---
+++ src/Devita.java
@@ -0,0 +1,16 @@
+
+public class Devita{
+
+	public static void main(String[] args) {
+		Frigo a=new Frigo("A",500);
+		Frigo b=new Frigo("A+",400);
+		Frigo c=new Frigo("A++",600);
+		a.Print();
+		b.Print();
+		c.Print();
+		a.Prezzo();
+		b.Prezzo();
+		c.Prezzo();
+	}
+
+}
